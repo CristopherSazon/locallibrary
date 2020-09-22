bf5ac6d114f7eaeddfbe0cdfdca9ba4dbbf61273
CodingChiefs <cris@codingchiefs.com>
2020-09-22 14:00:50 +0800

    create model admins

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book, BookInstance, Genre
+from .admins.book_admin import BookAdmin
+from .admins.author_admin import AuthorAdmin
+from .admins.book_instance_admin import BookInstanceAdmin
+from .admins.genre_admin import GenreAdmin
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
+admin.site.register(Genre, GenreAdmin)
---
+++ catalog/admins/book_admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+
+class BookAdmin(admin.ModelAdmin):
+    pass
