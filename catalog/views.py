0ca3914f9bdb38b5ae8c4851eb1efd37bd785e61
CodingChiefs <cris@codingchiefs.com>
2020-09-24 11:02:18 +0800

    add author list and author detail views

--- catalog/views.py
+++ catalog/views.py
@@ -23,8 +23,19 @@ class BookListView(generic.ListView):
     model = Book
     context_object_name = 'book_list'
     queryset = Book.objects.all()
-    paginate_by = 1
+    paginate_by = 5
 
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'
+    queryset = Author.objects.all()
+    paginate_by = 5
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
